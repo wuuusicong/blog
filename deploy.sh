c17883ee3d3d06e0f6534009c2edfed4ac806ec6
DaybydayToAspiration <wuuusicong@gmail.com>
2021-06-26 17:20:10 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,19 @@ set -e
 yarn build
 
 # cd 到构建输出的目录下
-cd dist
+#cd dist
 
 # 部署到自定义域域名
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
+#git init
+git add dist
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:wuuusicong/blog.git master:gh-pages
-
-cd -
+# git push -f git@github.com:wuuusicong/blog.git master:gh-pages
+ git subtree push --prefix=dist origin gh-pages
+#cd -
