40a2fedd1155e88a309ec833cfb6058bbee94b87
DaybydayToAspiration <wuuusicong@gmail.com>
2021-06-26 16:28:12 +0800

    dist

---
+++ dist/precache-manifest.cc7f617607b496ba714b9530caa8bcd3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b2873a80d2835fe6fd",
+    "url": "/blog/css/app.4dc7a6e6.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/blog/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "506aa69d8fd206e72c15015e4dfac020",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "377aa73b3091d29b5287",
+    "url": "/blog/js/about.76da0ce3.js"
+  },
+  {
+    "revision": "58b2873a80d2835fe6fd",
+    "url": "/blog/js/app.18e02971.js"
+  },
+  {
+    "revision": "d9ccb2d9a75476175fc6",
+    "url": "/blog/js/chunk-vendors.7f5cc242.js"
+  },
+  {
+    "revision": "69b649f3f7f4f8f6e1b3afed797a8459",
+    "url": "/blog/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/blog/robots.txt"
+  }
+]);
